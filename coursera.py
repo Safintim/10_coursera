57b0f7b808f64cf8b723b73afe66a7952e1738cb
Safin Timur <timurtlt96@mail.ru>
2019-07-25 22:50:47 +0300

    Added output file

--- coursera.py
+++ coursera.py
@@ -1,16 +1,71 @@
+import requests
+import argparse
+import json
+from lxml import etree
+from openpyxl import Workbook
+from bs4 import BeautifulSoup
 
 
-def get_courses_list():
-    pass
+def main():
+    parser = create_parser()
+    namespace = parser.parse_args()
+    URL_COURSES = 'https://www.coursera.org/sitemap~www~courses.xml'
+    try:
+        courses = get_courses(URL_COURSES)
+        courses_info = [get_course_info(course) for course in courses]
+    except requests.exceptions.RequestException as e:
+        exit(e)
+
+    output_courses_info_to_xlsx(namespace.filepath, courses_info)
 
 
-def get_course_info(course_slug):
-    pass
+def create_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('filepath', help='Path to file')
+    return parser
 
 
-def output_courses_info_to_xlsx(filepath):
-    pass
+def get_courses(url, count=20):
+    response = requests.get(url)
+    response.raise_for_status()
+    content = response.content
+    urlset = etree.XML(content)
+    return [loc.text for url in urlset[:count] for loc in url]
+
+
+def get_course_info(url_cource):
+    response = requests.get(url_cource)
+    response.raise_for_status()
+    content = response.text
+    page_content = BeautifulSoup(content, 'html.parser')
+
+    title_class = 'H2_1pmnvep-o_O-weightNormal_s9jwp5-o_O-fontHeadline_1uu0gyz max-text-width-xl m-b-1s'
+    language_class = 'H4_1k76nzj-o_O-weightBold_uvlhiv-o_O-bold_1byw3y2 m-b-0'
+    count_week_class = 'H1Xl_jd0thw-o_O-weightNormal_s9jwp5-o_O-fontHeadline_1uu0gyz text-secondary d-block m-y-1'
+    rating_class = 'H4_1k76nzj-o_O-weightBold_uvlhiv-o_O-bold_1byw3y2 m-l-1s m-r-1 m-b-0'
+
+    script_content = page_content.find('script', attrs={'type': 'application/ld+json'}).text
+    date_start = json.loads(script_content)['@graph'][1]['hasCourseInstance']['startDate']
+    return [
+        page_content.find(class_=title_class).text,
+        page_content.find(class_=rating_class).text,
+        page_content.find_all(class_=language_class)[-1].text,
+        date_start,
+        len(page_content.find_all(class_=count_week_class)),
+    ]
+
+
+def output_courses_info_to_xlsx(filepath, courses):
+    work_book = Workbook()
+    work_sheet = work_book.active
+    header = ['title', 'rating', 'language', 'date_start', 'count_week']
+    work_sheet.append(header)
+
+    for course in courses:
+        work_sheet.append(course)
+
+    work_book.save(filepath)
 
 
 if __name__ == '__main__':
-    pass
+    main()
